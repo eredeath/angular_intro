429fd7db13c9eb80c41d58b84f797f6f04f8d936
Ryan Jonesford <eredeath@users.noreply.github.com>
2016-06-11 22:32:45 -0400

    Tutorial step 5: Services Completed. getHeroesSlowly() is written in hero.service.ts, but not used.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core'
+import {OnInit} from '@angular/core'
 import {Hero} from './hero';
 import {HeroDetailComponent} from './hero-detail.component';
+import {HeroService} from './hero.service';
 
 @Component({
     selector: 'my-app',
@@ -60,26 +62,24 @@ import {HeroDetailComponent} from './hero-detail.component';
         borer-radius: 4px 0 0 4px;
     }
   `],
-  directives: [HeroDetailComponent]
+  directives: [HeroDetailComponent],
+  providers:[HeroService]
 })
 
-export class AppComponent { 
-    public heroes = HEROES;
+export class AppComponent implements OnInit{ 
     title = 'Tour of Heroes';
+    heroes: Hero[];
     selectedHero: Hero;
+
+    constructor(private heroService: HeroService){};
+    
+    getHeroes(){
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    };
+    
+    ngOnInit(){
+        this.getHeroes();
+    }
+    
     onSelect(hero: Hero){this.selectedHero = hero;}
 }
-
-var HEROES: Hero[] = [
-    { "id": 11, "name": "Mr. Nice" },
-    { "id": 12, "name": "Narco" },
-    { "id": 13, "name": "Bombasto"},
-    { "id": 14, "name": "Celeritas"},
-    { "id": 15, "name": "Magenta"},
-    { "id": 16, "name": "RubberMan"},
-    { "id": 17, "name": "Dynama"},
-    { "id": 18, "name": "Dr IQ"},
-    { "id": 19, "name": "Magma"},
-    { "id": 20, "name": "Tornado"}
-];
-
