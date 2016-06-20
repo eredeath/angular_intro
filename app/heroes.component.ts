c9ca7c6f4ae13de1261583d855f650efa518bb0a
Ryan Jonesford <eredeath@users.noreply.github.com>
2016-06-20 07:31:22 -0400

    Tutorial step 7 completed. The only thing that seems to be wrong is the delete button doesn't look right; wrong placement & color

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,5 +1,4 @@
-import {Component} from '@angular/core'
-import {OnInit} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
 import {Router} from '@angular/router-deprecated';
 import {Hero} from './hero';
 import {HeroDetailComponent} from './hero-detail.component';
@@ -8,21 +7,47 @@ import {HeroService} from './hero.service';
 @Component({
     selector: 'my-heroes',
     templateUrl:'app/heroes.component.html',
-    styleUrls: ['app/heroes.component.css']
+    styleUrls: ['app/heroes.component.css'],
+    directives: [HeroDetailComponent]
 })
 
 export class HeroesComponent implements OnInit{ 
-    title = 'Touring of Heroes';
     heroes: Hero[];
     selectedHero: Hero;
+    addingHero = false;
+    error: any;
 
     constructor(
         private router: Router,
-        private heroService: HeroService){};
+        private heroService: HeroService){}
     
     getHeroes(){
-        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-    };
+        this.heroService
+            .getHeroes()
+            .then(heroes => this.heroes = heroes)
+            .catch(error => this.error = error);
+    }
+
+    addHero(){
+        this.addingHero = true;
+        this.selectedHero = null;
+    }
+
+    close(savedHero: Hero){
+        this.addingHero = false;
+        if(savedHero){this.getHeroes();}
+    }
+
+    delete(hero: Hero, event: any){
+        event.stopPropagation();
+        this.heroService
+            .delete(hero)
+            .then(res=> {
+                this.heroes = this.heroes.filter(h=> h !== hero);
+                if(this.selectedHero == hero){this.selectedHero = null}
+            })
+            .catch(error => this.error = error);
+    }
     
     ngOnInit(){
         this.getHeroes();
